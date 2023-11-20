dd7ab0babe90575980ccb6e08c86dd92f5e988c1
vagne <vagnerpiresnh@hotmail.com>
2023-11-20 10:05:47 +0000

    update2

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -15,9 +15,9 @@ const PageNotFound = () => {
     <section class="w-820px">
 
         <p>Sorry, this page does not exist.</p>
-        <p>Return to the website <a href="/spa">Home</a> page.</p>
+        <p>Return to the website <a href="/">Home</a> page.</p>
 
-        <p>Return to the website <Link to="/spa">Home</Link> page.</p>
+        <p>Return to the website <Link to="/">Home</Link> page.</p>
     </section>
     </>
   );
